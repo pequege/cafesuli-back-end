f562a53e5631ceefb1bfb657921c393688d37fcc
Gerardo Mansilla <gerardoemansilla@gmail.com>
2023-07-05 00:29:24 -0300

    se terminaron las configuraciones iniciales

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 import express from "express";
-console.log("prueba b-e");
+import cors from "cors";
+import morgan from "morgan";
+import path from "path";
+//usar un puerto
+const app = express();
+
+app.set('port', process.env.PORT || 4000);
+app.listen(app.get('port'), ()=>{
+    console.log("estoy en el puerto " + app.get('port'));
+})
+
+//middlewares
+app.use(cors()); //permitir conexiones remotas
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, '/public')));
+//rutas
